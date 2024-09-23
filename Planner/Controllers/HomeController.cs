07e85d36a10ecf140d209242773fe1064e214e52
VinniGomes7 <141679025+VinniGomes7@users.noreply.github.com>
2024-09-23 02:00:43 -0300

    Adição das Metas na Home e correção na lógica das Metas

--- Planner/Controllers/HomeController.cs
+++ Planner/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Planner.Models;
+using Planner.Models.Enum;
 using Planner.Service;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -11,13 +12,17 @@ namespace Planner.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly LembreteService _lembreteService;
         private readonly TarefaService _tarefaService;
+        private readonly MetaService _metaService;
+
+
 
         // Injeta o serviço de lembretes
-        public HomeController(ILogger<HomeController> logger, LembreteService lembreteService, TarefaService tarefaService)
+        public HomeController(ILogger<HomeController> logger, LembreteService lembreteService, TarefaService tarefaService, MetaService metaService)
         {
             _logger = logger;
             _lembreteService = lembreteService;
             _tarefaService = tarefaService;
+            _metaService = metaService;
         }
 
         // Busca os lembretes a expirar e passa para a view
@@ -29,6 +34,24 @@ namespace Planner.Controllers
 
             var tarefasSemana = await _tarefaService.GetTarefasSemanaAsync();
             var tarefasOrdenadas = tarefasSemana.OrderBy(t => t.Dia).ThenBy(t => t.Inicio).ToList();
+
+            // Busca todas as metas
+            var metas = await _metaService.GetAllMetasAsync();
+            _logger.LogInformation($"Total de metas retornadas: {metas.Count()}");
+
+            // Log para verificar cada meta
+            foreach (var meta in metas)
+            {
+                _logger.LogInformation($"Meta: {meta.Titulo}, Prazo: {meta.Prazo.ToShortDateString()}");
+            }
+
+            // Filtra metas para hoje e próximos dias
+            ViewBag.MetasFuturas = metas
+                .Where(m => m.Prazo.Date >= DateTime.Today) // Exibe metas que ainda não passaram
+                .OrderBy(m => m.Prazo)
+                .ToList();
+
+
 
             return View(tarefasOrdenadas);
         }
